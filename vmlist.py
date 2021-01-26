f062468c4f934d472344520886bb5edcdee2b107
turnip_soup <jeremy.heckt@gmail.com>
2021-01-25 20:35:43 -0800

    initial commit

---
+++ vmlist.py
@@ -0,0 +1,56 @@
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
+import uuid
+
+# Connection function for each load
+def sqlcon():
+    connection = sqlite3.connect("./vm.db", check_same_thread=False)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+with sqlcon() as connection:
+    cursor = connection.cursor()
+
+    # Create sqlite db if it does not exist
+    try:
+        cursor.execute("CREATE TABLE vms (uuid TEXT, name TEXT, creator TEXT, purpose TEXT, ip TEXT, cpu_cores INTEGER, rammb INTEGER, os TEXT)")
+    except:
+        pass
+
+
+# Load flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = str(uuid.uuid4())
+
+# Root page
+@app.route("/")
+def listrender():
+    conn = sqlcon()
+    curr_vms = conn.execute("SELECT * FROM vms").fetchall()
+    return render_template('index.html', vms=curr_vms)
+
+@app.route("/<string:vmid>/delete", methods=["POST"])
+def delete(vmid):
+    with sqlcon() as conn:
+        conn.execute('DELETE FROM vms WHERE uuid = ?', (vmid,))
+        conn.execute("COMMIT")
+        flash(f"VM ID {vmid} was successfully deleted!")
+        return redirect(url_for('listrender'))
+
+@app.route("/add/", methods=["POST"])
+def add():
+    
+    with sqlcon() as conn:
+        ## Build our query
+        vmuuid = str(uuid.uuid4().hex.lower())
+        vmname = request.form['vmname']
+        creator = request.form['creator']
+        purpose = request.form['purpose']
+        ip = request.form['ipaddr']
+        cpu_cores = request.form['cpus']
+        rammb = request.form['rammb']
+        ops = request.form['ops']
+
+        conn.execute("""INSERT INTO vms(uuid, name, creator, purpose, ip, cpu_cores, rammb, os) VALUES(?,?,?,?,?,?,?,?)""", (vmuuid, vmname, creator, purpose, ip, cpu_cores, rammb, ops))
+        conn.commit()
+        return redirect(url_for('listrender'))
